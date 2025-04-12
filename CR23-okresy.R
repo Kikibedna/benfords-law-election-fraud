07e1e8f20f04af3e3868d44fb7e5c156b150d088
Kikibedna <95220904+Kikibedna@users.noreply.github.com>
2025-04-12 23:35:33 +0200

    add last digits, refactor, first graphics

--- CR23-okresy.R
+++ CR23-okresy.R
@@ -8,9 +8,10 @@ data_okresni <- load_csv(file.path(cfg$data, "CR-prezidentske-2023", "export (2)
 data_kraj <- load_csv(file.path(cfg$data, "CR-prezidentske-2023", "export (3).csv"))
 
 # validate data ----------------
-sum(data_obecni$celkem) # z CSU: 5 759 197 - melo byt odevzdano, bylo odevzdano na obecni urovni: 5 736 265 (kde je 23k?)
-sum(data_okresni$celkem) # 5 759 197
-(sum(data_kraj$celkem) - sum(data_obecni$celkem)) # 22932 rozdil co nam chybi 
+
+# sum(data_obecni$celkem) # z CSU: 5 759 197 - melo byt odevzdano, bylo odevzdano na obecni urovni: 5 736 265 (kde je 23k?)
+# sum(data_okresni$celkem) # 5 759 197
+# (sum(data_kraj$celkem) - sum(data_obecni$celkem)) # 22932 rozdil co nam chybi 
 
 radek <- data_okresni |> filter(Okres == "Zahraničí") # nalezeno chybejicich 23k hlasu <- 
 names(radek) <- names(data_obecni)
@@ -22,17 +23,90 @@ data_obecni <- rbind(data_obecni, (radek))
 
 numbers <- data_obecni$Andrej_Babis #unname(data_obecni$Petr_Pavel)))#, unname(data_obecni$Andrej_Babis))) #, unname(data_obecni$celkem)
 
-first_digits <- digits(numbers)
-first_two_digits <- digits(numbers,2)
-first_three_digits <- digits(numbers,3)
-first_four_digits <- digits(numbers,4)
-first_five_digits <- digits(numbers,5)
+first_digits <- digits(numbers, first = 1)
+first_two_digits <- digits(numbers,first = 2)
+# first_three_digits <- digits(numbers,first = 3)
+# first_four_digits <- digits(numbers,first = 4)
+# first_five_digits <- digits(numbers,first = 5)
 
 # x2 <- seq(min(first_sign_digits), max(first_sign_digits), length = 50)
-# fun <- P_BL_leading_digit(x2)
+# fun <- P_BL_FD(x2)
 # hist(first_sign_digits, freq = F, probability = T, breaks = 11) 
 # lines(x2, fun, col = 2, lwd = 2)
+plot_BL_RelFreq_bar(first_digits)
+save_png("CZ23-AB-first_digits")
 
-BL_leading_digit_table(first_digits) |> 
-  ggplot() + geom_bar(aes(x=digit, y=RelFreq, fill = T), stat = 'identity', position = 'dodge') + 
-    geom_bar(aes(x=digit, y=BL, , alpha = 0.5),  stat = 'identity', position = 'dodge')
+
+
+## chisq test -------
+
+table_obecniCR2 <- BL_leading_digit_table(first_digits) |> mutate(frac = (diff^2)/BL, 
+                                                                  ndpi = BL*Freq, 
+                                                                  npi = BL*Total, 
+                                                                  absdiff = Freq-npi)
+sum(table_obecniCR2$absdiff)
+
+# assumption of the chiqs test
+prod(table_obecniCR2$ndpi > 5) == 1 
+prod(table_obecniCR2$npi > 5) == 1 
+
+n <- table_obecniCR2$Total |> unique()
+
+G <- n * sum(table_obecniCR2$frac)
+alpha <- 0.05
+
+G > pchisq(q = 1-alpha, df = 8) # toto je confusing melo by to byt ze kdyz je G vetsi nez to chi tak zamitam null :) 
+
+chisq.test(x=table_obecniCR2$Freq, p=table_obecniCR2$BL, rescale.p = F)
+
+# je-li p-hodnota > alpha: nezamitam H0 
+# je-li p-hodnota <= alpha: zamitam H1
+
+# H0: empirical distribution follows the theoretical distr. 
+# H!: empirical distribution does not follow the theoretical distr. 
+# result: false, the G (test statistics) falls into the acceptance region, not the rejection region 
+# which means we reject the alternative hypothesis and we can say this distribiton follows the BL :) 
+
+# now on the pvalue <--
+
+
+# First two digits --------
+
+plot_BL_RelFreq_bar(first_two_digits)
+save_png("CZ23-AB-first_two_digits")
+
+
+compliance_test_chisq(first_two_digits)
+
+# First three digits --------
+
+plot_BL_RelFreq_bar(first_three_digits)
+
+
+table_obecniCR2 <-BL_leading_digit_table(first_three_digits) |> mutate(frac = (diff^2)/BL, 
+                                                                     ndpi = BL*Freq, 
+                                                                     npi = BL*Total, 
+                                                                     absdiff = Freq-npi)
+
+chisq.test(x=table_obecniCR2$Freq, p=table_obecniCR2$BL, rescale.p = T)
+
+# Last digits --------
+
+last_digits <- digits(numbers, last = 1)
+last_two_digits <- digits(numbers,last = 2)
+# last_three_digits <- digits(numbers,last = 3)
+# last_four_digits <- digits(numbers,last = 4)
+# last_five_digits <- digits(numbers,last = 5)
+
+BL_last_digit_table(last_digits)
+plot_BL_RelFreq_bar(last_digits, last = T)
+save_png("CZ23-AB-last_digits")
+compliance_test_chisq(last_digits, last = T)
+
+
+# Last two digits --------
+
+BL_last_digit_table(last_two_digits)
+plot_BL_RelFreq_bar(last_two_digits, last = T)
+save_png("CZ23-AB-last_two_digits")
+compliance_test_chisq(last_two_digits, last = T)
--- functions.R
+++ functions.R
@@ -15,15 +15,37 @@ data <- data |> mutate(Petr_Pavel = as.numeric(Petr_Pavel),
 return(data)
 }
 
+
 # process 
-digits <- function(vec, stop=1, start=1){ # vec to first digit numeric vec 
-  if(stop > 1){
-    dgts<-as.numeric(substr(x=as.character(vec), start=start, stop=stop))
-    return(dgts[(dgts>=10^(stop-1) & dgts< 10^(stop))])
+digits <- function(vec, first=0, last=0){ # vec to first digit numeric vec 
+  if(last>0){
+    return(as.numeric(substr(as.character(vec), 
+                      start = nchar(as.character(vec)) - (last-1), 
+                      stop = nchar(as.character(vec)))))
   }
-  else {
-    return(as.numeric(substr(x=as.character(vec), start=start, stop=stop)))
+  if(first>0){
+    if(first==1){
+      return(as.numeric(substr(x=as.character(vec), 
+                               start=1, 
+                               stop=1)))
+    }else {
+      dgts<-as.numeric(substr(x=as.character(vec), 
+                              start=1, 
+                              stop=first))
+      return(dgts[(dgts>=10^(first-1) & dgts< 10^(first))])
+    }
   }
+  if(first < 0 | last < 0) {
+    stop("params first or last must be >= 0")
+  }
+  if(first == 0 & last == 0){
+    stop("select first or last by setting one of the params > 0")
+  }
+}
+
+P_BL_FD <- function(x){ # Probability of a digit / Benford's Law First Digit Law  
+  #X any number (1-999999) works
+  log10(1 + 1/x)
 }
 
 BL_leading_digit_table <- function(vec){
@@ -31,12 +53,17 @@ BL_leading_digit_table <- function(vec){
   Total <- length(vec)
   data.frame(table('digit' = vec), Total) |> 
     mutate(RelFreq = Freq/Total, 
-           BL = log10(1 + (1/as.numeric(digit))), 
+           BL = P_BL_FDs(as.numeric(as.character(digit))),
            diff = RelFreq-BL)
 }
 
-P_BL_leading_digit <- function(x){
-  log10(1 + 1/x)
+BL_last_digit_table <- function(vec){
+  len <- str_length(as.character(vec[1])) 
+  Total <- length(vec)
+  data.frame(table('digit' = vec), Total) |> 
+  mutate(RelFreq = Freq/Total, 
+         BL = dunif(x = as.numeric(as.character(digit)), min = 0, max = 10^len),
+         diff = RelFreq-BL)
 }
 
 # analyze 
@@ -46,4 +73,57 @@ OMV <- function(X){ # OMV > 3 condition to use first digit BL
              OMV =  unname(log10(quantile(X, 0.9)) - log10(quantile(X, 0.1))),
              suitable = unname(log10(quantile(X, 0.9)) - log10(quantile(X, 0.1))>3))
 }
+
+compliance_test_chisq <- function(vec, last = F){
+  if(last){
+    BL_table <- BL_last_digit_table(vec)
+  } else{
+    BL_table <- BL_leading_digit_table(vec)
+  }
+  chisq.test(x=BL_table$Freq, p=BL_table$BL, rescale.p = F)
+}
+
+
+# graphics 
+
+save_png <- function(name, plot = last_plot(), width = 18, height = 9) { #tady by se hodilo dat credit do prace... 
+  base_dir <- getwd()
+  img_dir <- config::get("graphics") 
+  pth <- file.path(base_dir, img_dir, paste0(name, ".png"))
+  ggsave(plot = plot, 
+         pth, 
+         # device = "ragg_png",
+         width = width, height = height, units = "cm", dpi = 300)
+}
+
+plot_BL_RelFreq_bar_blend <- function(digits, last = F){ #ukladam, kdyby se mi zastesklo... 
+  if(last){
+    BL_table <- BL_last_digit_table(digits) 
+  }else {
+    BL_table <- BL_leading_digit_table(digits) 
+  }
+  
+  BL_table |> 
+    ggplot() + geom_bar(aes(x=digit, y=RelFreq), fill="cornflowerblue", stat = 'identity', position = 'dodge') + 
+    geom_bar(aes(x=digit, y=BL, alpha = 0.25), stat = 'identity', position = 'dodge') +
+    theme_minimal() + theme(legend.position = "none", )
+  
+}
+
+plot_BL_RelFreq_bar <- function(digits, last = F){
+  if(last){
+    BL_table <- BL_last_digit_table(digits) 
+  }else {
+    BL_table <- BL_leading_digit_table(digits) 
+  }
+  
+  BL_table |> select(-diff)|> pivot_longer(cols = c('RelFreq', 'BL'), names_to = "law", values_to = 'freq') |> 
+    ggplot(aes(x=digit, y=freq, fill = as.factor(law))) + geom_bar(stat = 'identity', position = 'dodge') + 
+    theme_minimal() + scale_fill_manual(values=c(BL = "black", RelFreq = "cornflowerblue"), name = NULL)
+  
+}
+
+
+
+
 
